4000b57c900cd9b71b3218685ae83cc6c911c8ed
Sapitorico <renzoyaques@gmail.com>
2023-03-25 17:54:01 -0300

    update detatimes

--- models/base_model.py
+++ models/base_model.py
@@ -13,18 +13,18 @@ class BaseModel:
         String(60), primary_key=True, unique=True, nullable=False
         )
     created_at = Column(
-        DateTime, default=datetime.utcnow, nullable=False, index=True
+        DateTime, default=datetime.utcnow, nullable=False
         )
     updated_at = Column(
-        DateTime, default=datetime.utcnow, nullable=False, index=True
+        DateTime, default=datetime.utcnow, nullable=False
         )
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
         else:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -34,9 +34,9 @@ class BaseModel:
             if 'id' not in kwargs:
                 self.id = str(uuid.uuid4())
             if 'created_at' not in kwargs:
-                self.created_at = datetime.now()
+                self.created_at = datetime.utcnow()
             if 'updated_at' not in kwargs:
-                self.updated_at = datetime.now()
+                self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -46,7 +46,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         storage.new(self)
         storage.save()
 

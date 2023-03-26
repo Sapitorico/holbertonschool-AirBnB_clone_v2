29665de776cc7b16a5ae9f5822f70a0e8c769461
Sapitorico <renzoyaques@gmail.com>
2023-03-26 19:16:11 -0300

    Add an instance place_amenity, class attribute amenities

--- models/place.py
+++ models/place.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 from models.review import Review
+from models.amenity import Amenity
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
+    place_amenity = Table('place_amenity', Base.metadata,
+                        Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                        Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+                        )
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -24,6 +29,7 @@ class Place(BaseModel, Base):
 
     if getenv('HBNB_ENV') == 'db':
         reviews = relationship("Review", cascade="all, delete, delete-orphan", backref="place")
+        amenities = relationship('Amenity', secondary=place_amenity, backref='places', viewonly=False)
     else:
         @property
         def reviews(self):
@@ -33,3 +39,21 @@ class Place(BaseModel, Base):
                 if rev.place_id == self.id:
                     review_list.append(rev)
             return review_list
+
+        @property
+        def amenities(self):
+            from models import storage
+            list_of_amenities = []
+            all_amenities = storage.all(Amenity)
+            for key, obj in all_amenities.items():
+                if key in self.amentiy_ids:
+                    list_of_amenities.append(obj)
+            return list_of_amenities
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            if type(obj).__name__ == 'Amenity':
+                new_amenity = 'Amenity' + '.' + obj.id
+                self.amenity_ids.append(new_amenity)
+            else:
+                pass

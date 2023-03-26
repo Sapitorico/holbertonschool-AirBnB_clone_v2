b5c77b3fddf76d59f1b0fb3f24980af36a57809d
Sapitorico <renzoyaques@gmail.com>
2023-03-26 20:00:53 -0300

    fix all tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -12,43 +12,34 @@ class TestDBStorage(unittest.TestCase):
     @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', 'only testing db storage')
     def test_all(self):
         """Test the all() method"""
-        # Create a new user and save it
         user = User(email="test@test.com", password="testpass")
         user.save()
-        # Test if the user is in the database
         objs = storage.all(User)
         self.assertIn(user, objs.values())
 
     @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', 'only testing db storage')
     def test_new(self):
         """Test the new() method"""
-        # Create a new user and save it
         user = User(email="test@test.com", password="testpass")
         storage.new(user)
-        # Test if the user is in the database
         objs = storage.all(User)
         self.assertIn(user, objs.values())
 
     @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', 'only testing db storage')
     def test_save(self):
         """Test the save() method"""
-        # Create a new user and save it
         user = User(email="test@test.com", password="testpass")
         storage.new(user)
         storage.save()
-        # Test if the user is in the database
         objs = storage.all(User)
         self.assertIn(user, objs.values())
 
     @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', 'only testing db storage')
     def test_delete(self):
         """Test the delete() method"""
-        # Create a new user and save it
         user = User(email="test@test.com", password="testpass")
         storage.new(user)
         storage.save()
-        # Delete the user
         storage.delete(user)
-        # Test if the user was removed from the database
         objs = storage.all(User)
         self.assertNotIn(user, objs.values())

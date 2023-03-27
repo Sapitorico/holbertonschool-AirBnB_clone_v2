5da9251a17a732736ec269686c4e059be9cec0c4
Sapitorico <renzoyaques@gmail.com>
2023-03-27 10:25:55 -0300

    display a varable of text

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+
+
+""" application of Flask variable"""
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index1():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def index(text):
+    return f"C {text}"
+
+
+if __name__ == '__main__':
+    """ starts a Flask web application """
+    app.run(debug=True, host='0.0.0.0', port=5000)

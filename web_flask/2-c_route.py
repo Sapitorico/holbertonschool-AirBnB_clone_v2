79b95a4b337b0322d0d6ea94f9ad7cf43f0f632f
Sapitorico <renzoyaques@gmail.com>
2023-03-27 10:43:30 -0300

    wtf codestyle

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,22 +9,28 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index1():
-    """document"""
+    """
+    document
+    """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def index2():
-    """document"""
+    """
+    document
+    """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def index3(text):
-    """text variable to be displayed on the route"""
-    return f"C {text}"
+    """
+    text variable to be displayed on the route
+    """
+    return f"C {text.replace('_', ' ')}"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
     """ starts a Flask web application """

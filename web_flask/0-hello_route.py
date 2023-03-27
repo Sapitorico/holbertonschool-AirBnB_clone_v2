e844f2d6b9f475ced3ac88e34e42201ee7d7132d
Sapitorico <renzoyaques@gmail.com>
2023-03-27 09:00:21 -0300

    add document

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" script that starts a Flask web application """
 from flask import Flask
 
+""" application of Flask variable"""
 app = Flask(__name__)
 
 
@@ -10,4 +12,5 @@ def index():
 
 
 if __name__ == '__main__':
+    """ starts a Flask web application """
     app.run(debug=True, host='0.0.0.0', port=5000)

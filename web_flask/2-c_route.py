732ce07cc18bdbee9d6fb5c85ca9168f605e24b5
Sapitorico <renzoyaques@gmail.com>
2023-03-27 10:40:07 -0300

    wtf codestyle

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,11 +9,13 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index1():
+    """document"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def index2():
+    """document"""
     return "HBNB"
 
 
@@ -24,5 +26,5 @@ def index3(text):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
     """ starts a Flask web application """

84c85785b9dc44dbcfa47c431e934b978b659e62
Sapitorico <renzoyaques@gmail.com>
2023-03-27 10:32:50 -0300

    add a comments

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """ script that starts a Flask web application """
 from flask import Flask
+"""text variable to be displayed on the route"""
 
 
 """ application of Flask variable"""
 app = Flask(__name__)
+"""text variable to be displayed on the route"""
 
 
 @app.route('/', strict_slashes=False)
 def index1():
+    """text variable to be displayed on the route"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def index():
+    """text variable to be displayed on the route"""
     return "HBNB"
 
 

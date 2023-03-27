1c7da26bb77d387653c51e19321de3d4b36ea7d3
Sapitorico <renzoyaques@gmail.com>
2023-03-27 10:37:09 -0300

    wtf codestyle

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """ script that starts a Flask web application """
 from flask import Flask
-"""text variable to be displayed on the route"""
 
 
 """ application of Flask variable"""
 app = Flask(__name__)
-"""text variable to be displayed on the route"""
 
 
 @app.route('/', strict_slashes=False)
 def index1():
-    """text variable to be displayed on the route"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def index():
-    """text variable to be displayed on the route"""
     return "HBNB"
 
 
@@ -28,5 +24,5 @@ def index(text):
 
 
 if __name__ == '__main__':
-    """ starts a Flask web application """
     app.run(debug=True, host='0.0.0.0', port=5000)
+    """ starts a Flask web application """

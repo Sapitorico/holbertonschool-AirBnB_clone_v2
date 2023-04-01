7e5adb7a59a207d0cef1933ab2020e240b867885
Sapitorico <renzoyaques@gmail.com>
2023-04-01 15:07:08 -0300

    update

--- models/__init__.py
+++ models/__init__.py
@@ -4,15 +4,15 @@ from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
 from os import getenv
 
-class Base():
-    pass
+# class Base():
+#     pass
 
-from models.user import User
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.state import State
-from models.review import Review
+# from models.user import User
+# from models.amenity import Amenity
+# from models.city import City
+# from models.place import Place
+# from models.state import State
+# from models.review import Review
 
 
 if getenv("HBNB_TYPE_STORAGE") == "db":

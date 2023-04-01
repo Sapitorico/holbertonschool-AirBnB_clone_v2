887eaf70e9b9e3f8a9c0c13710f0f60919c4ea2b
Sapitorico <renzoyaques@gmail.com>
2023-04-01 11:57:20 -0300

    sdadas

--- models/__init__.py
+++ models/__init__.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-# from models.base_model import BaseModel
+from models.base_model import BaseModel
 from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
 from os import getenv
 
-# class Base():
-#     pass
+class Base():
+    pass
 
-# from models.user import User
-# from models.amenity import Amenity
-# from models.city import City
-# from models.place import Place
-# from models.state import State
-# from models.review import Review
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
 
 
 if getenv("HBNB_TYPE_STORAGE") == "db":

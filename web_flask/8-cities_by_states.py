4c905ef5a17d4eefc8c7f2c15a111ba4f7c65356
Sapitorico <renzoyaques@gmail.com>
2023-04-02 17:36:04 -0300

    no time

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
 """ Task 8 module """
+
+from os import getenv
 from flask import Flask, render_template
-from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """Displays states and their cities"""
+def state_city_print():
+    """odcast"""
 
-    states_dict = storage.all(State)
+    from models import storage
+    from models.state import State
 
+    states_dict = storage.all(State).values()
     states_city_dict = {}
-    for state in states_dict.values():
-        states_city_dict[state] = sorted(state.cities, key=lambda c: c.name)
 
-    states_list = sorted(states_dict.values(), key=lambda s: s.name)
+    for state in states_dict:
+        states_city_dict[state] = state.cities
 
     return render_template('8-cities_by_states.html',
-                           states_list=states_list,
                            states_city_dict=states_city_dict)
 
 
 @app.teardown_appcontext
-def close_session(exception):
-    """Closes SQLAlchemy session"""
+def closing(dummy):
+    """penet"""
+
+    from models import storage
+
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)

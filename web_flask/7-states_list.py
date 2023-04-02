ba5c4b36896ec2f44c001240848c7c7070c72925
Sapitorico <renzoyaques@gmail.com>
2023-04-02 15:14:56 -0300

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,19 +3,17 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
 app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = list(storage.all(State).values())
-    states.sort(key=lambda state: state.name)
-    return render_template('7-states.html', states=states)
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 @app.teardown_appcontext
 def close_session(response_or_exc):
